3136de4baaf2d5244f423c8ad9c09b007ff8419f
Goran Pavlovic <gp.goranpavlovic@gmail.com>
2019-01-11 15:13:26 -0500

    Completed overview section

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,21 @@ import './index.css';
 
 class Square extends React.Component
 {
+    constructor (props)
+    {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render()
     {
         return (
-            <button className="square">
-                {/* TODO */ }
+            <button
+                className="square"
+                onClick={ () => this.setState({ value: 'X' }) }>
+                { this.state.value }
             </button>
         );
     }
@@ -18,7 +28,7 @@ class Board extends React.Component
 {
     renderSquare(i)
     {
-        return <Square />;
+        return <Square value={ i } />;
     }
 
     render()
